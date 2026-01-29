c4c932d316b2815e3d6e7c588e83574d0d931415
Shadman Sami Shanon <shadmansami3s@gmail.com>
2026-01-29 11:49:58 +0600

    Update

--- server/order_service/app.py
+++ server/order_service/app.py
@@ -1,14 +1,8 @@
 from flask import Flask, jsonify
-from flask_jwt_extended import JWTManager
-from datetime import timedelta
 import logging
 import os
 
 app = Flask(__name__)
-
-app.config["JWT_SECRET_KEY"] = os.environ["SECRET_KEY"] 
-app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(days = 1) 
-jwt = JWTManager(app)
 
 logging.basicConfig (
     filename = "app.log",
@@ -24,5 +18,9 @@ def home():
 def health():
     return jsonify({"status": "ok"}), 200 
 
+# @app.route("/alert")
+# def alert():
+    
+
 if __name__ == "__main__":
-    app.run(host = "0.0.0.0", port = 8001, debug = True)
+    app.run(host = "0.0.0.0", port = 8000, debug = True)
