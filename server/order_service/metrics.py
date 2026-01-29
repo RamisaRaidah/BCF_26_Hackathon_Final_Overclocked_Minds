cd6ea80cf8889404d2307ecb36c52324e3acd9db
Shadman Sami Shanon <shadmansami3s@gmail.com>
2026-01-29 17:43:15 +0600

    Yaaayy

--- server/order_service/metrics.py
+++ server/order_service/metrics.py
@@ -5,7 +5,7 @@ _WINDOW_SECONDS = 30
  
 class RollingLatency:
     def __init__(self):
-        self.samples = deque()  # (timestamp, latency_ms)
+        self.samples = deque()  
  
     def add(self, latency_ms: int):
         now = time.time()
@@ -25,5 +25,4 @@ class RollingLatency:
         return sum(ms for _, ms in self.samples) / len(self.samples)
  
     def status(self) -> str:
-        # Red if avg > 1000ms
         return "red" if self.avg_ms() > 1000 else "green"
